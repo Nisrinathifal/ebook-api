ae1539bcb0f86c34a2d1653fea0f9b33820786db
Icha Nisa <icha71651@gmail.com>
2020-09-01 21:38:10 +0700

    Upload Author

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,20 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    public function me(){
-        return "bebas;";
-    
+    public function me()
+    {
+        return
+        [
+            "NIS" => 3103118101,
+            "Name" => "Nisrina Thifal Khairunnisa",
+            "Gender" => "Perempuan",
+            "Phone" => 6285714591232,
+            "Class" => "XII RPL 3",
+
+        ];
     }
 }
